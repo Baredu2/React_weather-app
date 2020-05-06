984c94cc895409da55d1972ca610b8fac7e67c7e
Krzysztof Eron <eronkrzysztof@gmail.com>
2020-05-06 18:31:19 +0200

    first commit

---
+++ src/components/Todayinfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Todayinfo = () => {
+    return(
+        <div className="container">
+            <div className="cards">
+                <h1>Today Weather</h1>
+                <i className="wi wi-day-sunny display-1000"/> 
+            </div>
+        </div>
+    )
+}
+
+export default Todayinfo;
