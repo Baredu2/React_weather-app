95d68343759611d0cc842def69b58b24af916743
Krzysztof Eron <eronkrzysztof@gmail.com>
2020-05-07 14:49:56 +0200

    commit #2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import 'weather-icons-lite/css/weather-icons-lite.css'
-import Todayinfo from'./components/Todayinfo'
+import Todayinfo from'./components/TodayWeather'
+
+
+
+
 class App extends React.Component{
+  city = 'London';
+  country = 'Uk';
   render(){
     return (
         <div>
-            <Todayinfo/>
+            <Todayinfo city={this.city} country={this.country}/>
         </div>
     )
    };
-
-  }
+}
 export default App;
---
+++ src/components/TodayWeather.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const request = require('request');
+const weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=Harrow,uk&appid=f3ed895a5eb70251dfdb338f042a8069';
+const options = {json: true};
+
+function getWeather(city,country){
+    console.log(city,country);
+    request(weather_url, options, (error, res, body) => {
+        if (error) {
+            console.log(error)
+        };
+    
+        if (!error && res.statusCode === 200) {
+            console.log(body);
+            return body;
+        };
+    });
+};
+const TodayWeather = (props) => {
+    const json_weather = JSON.stringify(getWeather(props.city,props.country));
+    const cos = 'sddsds';
+    return(
+        <div className="today-weather-div">
+            <div>
+                <h1>Today Weather in {props.city},{props.country}</h1>
+                <i className="wi-10x wi wi-darksky-clear-day"/> 
+                <h2>{json_weather}</h2>
+            </div>
+        </div>
+    )
+}
+
+
+export default TodayWeather;
